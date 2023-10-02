6962b6fcc7ba5c184d9159ad8879e282cd23fec4
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-10-02 23:48:38 +0300

    sprint 2 complete

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,118 @@
 // ISD-22 Tischenko Danylo
-// Sprint 1
+// Sprint 2
 
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.print("Enter min: ");
-        int min = console.nextInt();
-        System.out.print("Enter max: ");
-        int max = console.nextInt();
+        int[] array = { 1, 2, 3, 3, 4, 4, 5, 23, 4, 2, 3 };
 
-        if (min > max) {
-            System.out.println("Min is greater than max");
-            return;
-        }
+        System.out.println("Input array: " + Arrays.toString(array));
+        System.out.println("Input value: 2");
 
-        int[] array = new int[max - min + 1];
+        System.out.println("Last index of value: " + lastIndexOf(array, 2));
+        System.out.println("All indexes of value: " + Arrays.toString(allIndexesOf(array, 2)));
+
+        int[] sortedArray = {1, 2, 3, 18, 19, 45, 85, 100};
+
+        System.out.println("Is sorted: " + isSorted(sortedArray));
+        System.out.println("Is sorted (known false): " + isSorted(array));
+
+        System.out.println("Mode: " + mode(array));
+
+        int[] evenOddArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("Even before odd: " + Arrays.toString(evenBeforeOdd(evenOddArray)));
+
+    }
+
+    public static int lastIndexOf(int[] array, int value) {
+        int index = -1;
+
         for (int i = 0; i < array.length; i++) {
-            array[i] = min + i;
-        }
-
-        System.out.println("Array: " + Arrays.toString(array));
-        System.out.println("Count: " + array.length);
-
-        int[] oddArray = new int[array.length];
-        int oddCount = 0;
-        int[] evenArray = new int[array.length];
-        int evenCount = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                oddArray[oddCount] = array[i];
-                oddCount++;
-            } else {
-                evenArray[evenCount] = array[i];
-                evenCount++;
+            if (array[i] == value) {
+                index = i;
             }
         }
 
-        System.out.println("Odd array: " + Arrays.toString(oddArray));
-        System.out.println("Odd count: " + oddCount);
+        return index;
+    }
 
-        System.out.println("Even array: " + Arrays.toString(evenArray));
-        System.out.println("Even count: " + evenCount);
-
-        int[] negativeArray = new int[array.length];
-        int negativeCount = 0;
+    public static int[] allIndexesOf(int[] array, int value) {
+        int[] indexes = new int[0];
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
-                negativeArray[negativeCount] = array[i];
-                negativeCount++;
+            if (array[i] == value) {
+                indexes = Arrays.copyOf(indexes, indexes.length + 1);
+                indexes[indexes.length - 1] = i;
             }
         }
 
-        if (negativeCount > 0) {
-            System.out.println("Negative array: " + Arrays.toString(negativeArray));
-            System.out.println("Negative count: " + negativeCount);
-        } else {
-            System.out.println("Negative array is empty");
-        }
+        if(indexes.length == 0) return null;
 
-        int[] positiveArray = new int[array.length];
-        int positiveCount = 0;
+        return indexes;
+    }
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0) {
-                positiveArray[positiveCount] = array[i];
-                positiveCount++;
+    public static boolean isSorted(int[] array) {
+        boolean isSorted = true;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i + 1] < array[i]) {
+                isSorted = false;
+                break;
             }
         }
 
-        if (positiveCount > 0) {
-            System.out.println("Positive array: " + Arrays.toString(positiveArray));
-            System.out.println("Positive count: " + positiveCount);
-        } else {
-            System.out.println("Positive array is empty");
+        return isSorted;
+    }
+
+    public static int mode(int[] array) {
+        if (array.length == 0) return -1;
+        if (array.length == 1) return array[0];
+
+        int[] frequency = new int[101];
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] >= 0 && array[i] <= 100) {
+                frequency[array[i]]++;
+            }
         }
+
+        int mostFrequent = 0;
+
+        for (int i = 0; i < frequency.length; i++) {
+            int newFreq = frequency[i];
+            int oldFreq = frequency[mostFrequent];
+
+            if(newFreq > oldFreq) {
+                mostFrequent = i;
+                continue;
+            }
+
+            if(newFreq == oldFreq) {
+                mostFrequent = Math.min(mostFrequent, i);
+            }
+        }
+
+        return mostFrequent;
+    }
+
+    public static int[] evenBeforeOdd(int[] array) {
+        int evenIndex = 0;
+        int oddIndex = array.length - 1;
+
+        for (; evenIndex < oddIndex; evenIndex++) {
+            if(array[evenIndex] % 2 == 1) {
+                for (; oddIndex > evenIndex; oddIndex--) {
+                    if(array[oddIndex] % 2 == 0) {
+                        int temp = array[evenIndex];
+                        array[evenIndex] = array[oddIndex];
+                        array[oddIndex] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return array;
     }
 }
