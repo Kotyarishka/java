e6d259946233aeef992e135278d123897bed413b
Danylo <44447172+Kotyarishka@users.noreply.github.com>
2023-12-11 23:50:24 +0000

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,24 +1,19 @@
-//Напишіть клас Rectangle, який описує прямокутник на площині.
-//Конструктор об'єктів: Rectangle(int sideA, int sideB).
-//Об'єкт має виводити інформацію про себе у виді площі прямокутника.
-//Забезпечити упорядкування об'єктів за природнім порядком від меншого до
-
-//більшого. Протестувати програму.
-
 public class Rectangle implements Comparable<Rectangle> {
     private int sideA;
     private int sideB;
+    private int area;
 
     public Rectangle(int sideA, int sideB) {
         this.sideA = sideA;
         this.sideB = sideB;
+        this.area = sideA * sideB
     }
 
     @Override
     public int compareTo(Rectangle other) {
-        if (this.sideA * this.sideB > other.sideA * other.sideB) {
+        if (this.area > other.area) {
             return 1;
-        } else if (this.sideA * this.sideB < other.sideA * other.sideB) {
+        } else if (this.area < other.area) {
             return -1;
         } else {
             return 0;
@@ -27,6 +22,6 @@ public class Rectangle implements Comparable<Rectangle> {
 
     @Override
     public String toString() {
-        return this.sideA * this.sideB + "";
+        return this.area + "";
     }
 }
